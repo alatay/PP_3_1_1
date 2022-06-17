e8b14abe530b3c55bebf88d4198b6f88339c71bb
Natali Knyazeva <knyazeva14@mail.ru>
2022-06-17 19:00:28 +0700

    Create User.java

---
+++ src/main/java/com/example/model/User.java
@@ -0,0 +1,64 @@
+package com.example.model;
+
+import org.hibernate.annotations.Check;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Check(constraints = "age > 0")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "firstname", unique = true)
+    private String firstName;
+
+    @Column(name = "lastname")
+    private String lastName;
+
+    @Column(name = "age")
+    private int age;
+
+    public User() {}
+
+    public User(String firstName, String lastName, int age) {
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setAge(age);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
